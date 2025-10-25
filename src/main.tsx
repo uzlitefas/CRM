3ee897c12489ba3a06426ecaed705f3d648304af
mahmudjooo <fusmonova818@gmail.com>
2025-10-25 17:59:16 +0500

    student Profile and studentAttendance update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app.tsx";
-import { BrowserRouter } from "react-router-dom";
+
+import StudentPage from "./pages/studentPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StudentPage />
   </StrictMode>
 );
