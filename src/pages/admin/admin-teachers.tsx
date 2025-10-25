0578165332e3632a1d897810db80f62ff77a6bdd
701alisherjan <701alisherjan@gmail.com>
2025-10-25 17:23:39 +0500

    charts sections

---
+++ src/pages/admin/admin-teachers.tsx
@@ -0,0 +1,5 @@
+function AdminTeachers() {
+  return <div>AdminTeachers</div>;
+}
+
+export default AdminTeachers;
--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,10 +1,11 @@
+import ChartsSection from "./_components/chart-section";
 
 function Admin() {
   return (
-    <div>
-      
+    <div className="flex-1  p-6 overflow-auto">
+      <ChartsSection />
     </div>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
